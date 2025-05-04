ebd5757471c5d185f011cb921105e51523cb49f1
Armaan <armaanzeyad@gmail.com>
2025-05-05 01:39:46 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from typing import Union
+import whisper
+from fastapi import FastAPI, File, UploadFile
+
+app = FastAPI()
+
+@app.post("/")
+async def transcribe_audio(file: UploadFile = File(...)):
+    # Save the uploaded file
+    with open("temp_audio_file.m4a", "wb") as f:
+        f.write(await file.read())
+    
+    # Load Whisper model
+    model = whisper.load_model("tiny")
+    
+    # Transcribe the saved audio file
+    result = model.transcribe("temp_audio_file.m4a")
+    print(result["text"])
+    
+    # Return the transcription result
+    return {"transcription": result["text"]}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
