3d1ede6061b7ac38ebe93bc92cb879685542cd52
Armaan <armaanzeyad@gmail.com>
2025-05-11 13:57:33 +0530

    do it

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Union
 import whisper
 from fastapi import FastAPI, File, UploadFile
+import uvicorn  
 
 app = FastAPI()
 
@@ -23,3 +24,6 @@ async def transcribe_audio(file: UploadFile = File(...)):
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
